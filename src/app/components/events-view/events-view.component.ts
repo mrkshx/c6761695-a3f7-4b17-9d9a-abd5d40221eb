91bdb756e5cc0cdab098cfb7e21bb187631113e2
Markus Mengel <markus.mengelhx@gmail.com>
2022-11-04 20:39:48 +0100

    displays actual time range

--- src/app/components/events-view/events-view.component.ts
+++ src/app/components/events-view/events-view.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { EventsDataService } from 'src/app/services/events-data.service';
 
 @Component({
   selector: 'app-events-view',
   templateUrl: './events-view.component.html',
-  styleUrls: ['./events-view.component.sass']
+  styleUrls: ['./events-view.component.sass'],
 })
-export class EventsViewComponent implements OnInit {
-
-  constructor(private eventsData: EventsDataService) { }
+export class EventsViewComponent implements OnInit, OnChanges {
+  constructor(public eventsData: EventsDataService) {}
+  firstDate = '';
+  lastDate = '';
 
   ngOnInit(): void {
-    this.eventsData.fetchEventsData()
+    this.getDateRange();
   }
 
+  ngOnChanges(): void {}
+
+  getDateRange() {
+    this.eventsData.datesSubject.subscribe((dates) => {
+      this.firstDate = dates[0]
+      this.lastDate = dates[dates.length - 1]
+    });
+  }
 }
