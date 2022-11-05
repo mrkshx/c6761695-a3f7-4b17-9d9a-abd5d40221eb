a2a51a4fe6eb6cec868bd283362b0e5d33ce88f2
Markus Mengel <markus.mengelhx@gmail.com>
2022-11-05 20:33:50 +0100

    adds new component shoppingcart to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsViewComponent } from './components/events-view/events-view.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 const routes: Routes = [
   { path: 'events', component: EventsViewComponent },
+  { path: 'shoppingcart', component: ShoppingCartComponent },
   { path: '', redirectTo: '/events', pathMatch: 'full' }
 
 ];
