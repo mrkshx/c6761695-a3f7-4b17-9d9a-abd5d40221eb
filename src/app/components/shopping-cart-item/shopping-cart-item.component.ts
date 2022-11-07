64f80a85f51ee8eb828ad7bbb8763dd9c5165572
Markus Mengel <markus.mengelhx@gmail.com>
2022-11-07 17:43:19 +0100

    passing data to shopping-cart-item

--- src/app/components/shopping-cart-item/shopping-cart-item.component.ts
+++ src/app/components/shopping-cart-item/shopping-cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-cart-item',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart-item.component.sass']
 })
 export class ShoppingCartItemComponent implements OnInit {
+  @Input() id: string = ''
+  @Input() title: string = ''
+  @Input() venueName: string = ''
+  @Input() date: string = ''
+
 
   constructor() { }
 
